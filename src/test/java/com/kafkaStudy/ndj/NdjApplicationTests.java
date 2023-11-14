7b0bbbd3e83f37f0780af137812b53503621abf7
DongJu-Na <ehdeld123@naver.com>
2023-11-14 18:08:19 +0900

    init project

---
+++ src/test/java/com/kafkaStudy/ndj/NdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaStudy.ndj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
