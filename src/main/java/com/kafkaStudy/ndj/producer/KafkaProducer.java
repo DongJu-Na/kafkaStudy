eab1e5a1f806ee695c642b59392def21d3abcb73
DongJu-Na <79893048+DongJu-Na@users.noreply.github.com>
2024-01-18 16:30:18 +0900

    client setting

--- src/main/java/com/kafkaStudy/ndj/producer/KafkaProducer.java
+++ src/main/java/com/kafkaStudy/ndj/producer/KafkaProducer.java
@@ -1,21 +1,15 @@
 package com.kafkaStudy.ndj.producer;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaProducer {
 
     private static final String TOPIC = "testTopic";
     private final  KafkaTemplate<String, String> kafkaTemplate;
-
-    @Autowired
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate){
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     public void sendMessgae(String message) {
         System.out.printf("Produce message : %s%n", message);
