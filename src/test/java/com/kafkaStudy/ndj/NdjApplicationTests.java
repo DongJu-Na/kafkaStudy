838a565be30f0d9e52b43ac5e08634a987418d40
DongJu-Na <79893048+DongJu-Na@users.noreply.github.com>
2024-02-13 14:27:19 +0900

    임시 테스트 코드

--- src/test/java/com/kafkaStudy/ndj/NdjApplicationTests.java
+++ src/test/java/com/kafkaStudy/ndj/NdjApplicationTests.java
@@ -1,13 +1,18 @@
 package com.kafkaStudy.ndj;
 
+import com.kafkaStudy.ndj.producer.KafkaProducer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NdjApplicationTests {
 
+	@Autowired
+	private KafkaProducer kafkaProducer;
 	@Test
 	void contextLoads() {
+		kafkaProducer.sendMessgae("카프카 테스트");
 	}
 
 }
