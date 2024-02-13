61f1dc88f76740450599f3038b288e7f4e9fd887
DongJu-Na <79893048+DongJu-Na@users.noreply.github.com>
2024-02-13 12:57:38 +0900

    프로듀서에서 메세지 보낼 시 엔티티 데이터 insert

--- src/main/java/com/kafkaStudy/ndj/producer/KafkaProducer.java
+++ src/main/java/com/kafkaStudy/ndj/producer/KafkaProducer.java
@@ -1,8 +1,14 @@
 package com.kafkaStudy.ndj.producer;
 
+import com.kafkaStudy.ndj.entity.Message;
+import com.kafkaStudy.ndj.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
@@ -10,9 +16,26 @@ public class KafkaProducer {
 
     private static final String TOPIC = "testTopic";
     private final  KafkaTemplate<String, String> kafkaTemplate;
+    private final MessageRepository messageRepository;
 
     public void sendMessgae(String message) {
         System.out.printf("Produce message : %s%n", message);
-        this.kafkaTemplate.send(TOPIC, message);
+
+
+        CompletableFuture<SendResult<String, String>> future = this.kafkaTemplate.send(TOPIC, message);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                System.out.println("Message sent successfully > " + result);
+                Message messageEntity = Message.builder()
+                        .message(result.getProducerRecord().value())
+                        .regDttm(LocalDateTime.now())
+                        .regUserId("api")
+                        .build();
+
+                messageRepository.save(messageEntity);
+            } else {
+                System.out.println("Error sending message " + ex);
+            }
+        });
     }
 }
